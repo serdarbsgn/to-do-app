c7f553198f2461efdf007948d76415e8b7225f98
serdarbsgn <serdar.bsgn@gmail.com>
2020-07-18 23:47:36 +0300

    n-n relation complete

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,4 @@
 class Todo < ApplicationRecord
-has_and_belongs_to_many :categories
 has_many :comments , dependent: :destroy
 validates :title, presence: true, length: {minimum:5}
 validates :description, presence: true, length: {minimum:5}
